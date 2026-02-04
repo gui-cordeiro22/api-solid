0bcf19dc759792c3845b8a74b186bf5cb8e7d043
Guilherme Cordeiro <guilhermecordeiro43@gmail.com>
2026-02-04 14:15:23 -0300

    feat: adding initial structures to connect with postgres database using docker

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,11 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+prisma.user.create({
+    data: {
+        name: "Guilherme Cordeiro",
+        email: "guilhermecordeiro43@gmail.com",
+    },
+});
+
 export const app = fastify();
